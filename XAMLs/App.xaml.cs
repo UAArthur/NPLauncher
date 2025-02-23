4336c0a52e4fd7b193cd3951263962e0689ba384
UAArthur <merklingerarthur@gmail.com>
2025-02-23 15:39:05 +0100

    fix: handle startup exceptions and improve tray icon management

--- XAMLs/App.xaml.cs
+++ XAMLs/App.xaml.cs
@@ -14,60 +14,67 @@ public partial class App : Application
 
     protected override async void OnStartup(StartupEventArgs e)
     {
-        base.OnStartup(e);
-
-        // Check if Launcher is already running
-        const string appName = "BPLauncher";
-        _mutex = new Mutex(true, appName, out var createdNew);
-        if (!createdNew)
+        try
         {
-            Current.Shutdown();
-            return;
-        }
+            base.OnStartup(e);
+
+            // Check if Launcher is already running
+            const string appName = "BPLauncher";
+            _mutex = new Mutex(true, appName, out var createdNew);
+            if (!createdNew)
+            {
+                Current.Shutdown();
+                return;
+            }
        
-        // Init AppSettings
-        var appSettings = new AppSettings();
-        await LanguageHandler.LoadLanguagesAsync();
+            // Init AppSettings
+            var appSettings = new AppSettings();
+            await LanguageHandler.LoadLanguagesAsync();
 
-        // Initialize Logger and Console
-        ConsoleHelper.ShowConsole();
-        Logger.Debug("Console initialized.");
+            // Initialize Logger and Console
+            ConsoleHelper.ShowConsole();
+            Logger.Debug("Console initialized.");
 
-        // Initialize Tray Icon
-        _trayIcon = (TaskbarIcon)FindResource("TrayIcon");
-        _trayIcon.TrayLeftMouseDown += TrayIcon_Click;
-        _trayIcon.TrayMouseDoubleClick += TrayIcon_Click;
-        Logger.Info("Application started, tray icon initialized.");
+            // Initialize Tray Icon
+            _trayIcon = (TaskbarIcon)FindResource("TrayIcon");
+            _trayIcon.TrayLeftMouseDown += TrayIcon_Click;
+            _trayIcon.TrayMouseDoubleClick += TrayIcon_Click;
+            Logger.Info("Application started, tray icon initialized.");
 
-        // Register Protocol
-        if (LauncherRegistryHandler.IsFirstStart())
-        {
-            LauncherRegistryHandler.RegisterProtocol();
-            LauncherRegistryHandler.SetFirstStartDone();
+            // Register Protocol
+            if (LauncherRegistryHandler.IsFirstStart())
+            {
+                LauncherRegistryHandler.RegisterProtocol();
+                LauncherRegistryHandler.SetFirstStartDone();
+            }
+
+            Logger.Info("Loading accounts...");
+            await appSettings.LoadAccountsAsync();
+
+            if (AppSettings.GetAccounts()!.Accounts.Count == 0)
+            {
+                Logger.Debug("No valid account found, opening AuthWindow.");
+                Current.MainWindow = new AuthWindow();
+            }
+            else
+            {
+                Logger.Debug("Valid account found, opening MainWindow.");
+                Current.MainWindow = new MainWindow();
+            }
+
+            Current.MainWindow.Show();
+
+            // Handle URI
+            if (e.Args.Length > 0)
+            {
+                var combinedArgs = string.Join(" ", e.Args);
+                Logger.Info($"Combined Arguments: {combinedArgs}");
+                UriHandlers.Handle(combinedArgs);
+            }
         }
-
-        Logger.Info("Loading accounts...");
-        await appSettings.LoadAccountsAsync();
-
-        if (AppSettings.GetAccounts()!.Accounts.Count == 0)
+        catch (Exception ex)
         {
-            Logger.Debug("No valid account found, opening AuthWindow.");
-            Current.MainWindow = new AuthWindow();
-        }
-        else
-        {
-            Logger.Debug("Valid account found, opening MainWindow.");
-            Current.MainWindow = new MainWindow();
-        }
-
-        Current.MainWindow.Show();
-
-        // Handle URI
-        if (e.Args.Length > 0)
-        {
-            var combinedArgs = string.Join(" ", e.Args);
-            Logger.Info($"Combined Arguments: {combinedArgs}");
-            UriHandlers.Handle(combinedArgs);
+            Logger.Error("An error occurred during startup. \n" + ex);
         }
     }
 
@@ -89,7 +96,7 @@ public partial class App : Application
 
         Logger.Debug("No visible window found, deciding which window to open.");
 
-        if (Current.MainWindow == null || Current.MainWindow is AuthWindow)
+        if (Current.MainWindow is null or AuthWindow)
         {
             Logger.Debug("Opening MainWindow.");
             Current.MainWindow = new MainWindow();
@@ -107,18 +114,32 @@ public partial class App : Application
     {
         Logger.Debug("Exit clicked, shutting down app.");
         _trayIcon?.Dispose();
+        _trayIcon = null;
         Current.Shutdown();
     }
+
 
     private void TrayIcon_Open_Click(object sender, RoutedEventArgs e)
     {
         var activeWindow = Current.Windows.OfType<Window>().FirstOrDefault(w => w.IsVisible);
 
-        if (activeWindow != null)
-        {
-            activeWindow.WindowState = WindowState.Normal;
-            activeWindow.ShowInTaskbar = true;
-            activeWindow.Activate();
-        }
+        if (activeWindow == null) return;
+        activeWindow.WindowState = WindowState.Normal;
+        activeWindow.ShowInTaskbar = true;
+        activeWindow.Activate();
     }
+    
+    protected override void OnExit(ExitEventArgs e)
+    {
+        Logger.Debug("Application exiting, disposing tray icon.");
+        _trayIcon?.Dispose();
+        _trayIcon = null;
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    
+        base.OnExit(e);
+    }
+
+
 }
